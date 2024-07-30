55293d179f4a57ea24af049f5534ac1e4428e43a
Efaz Ahmed <efazahmed.com@gmail.com>
2024-07-30 19:17:18 +0600

    Create armstrong-number.c

---
+++ armstrong-number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("Armstrong Numbers Are: ");
+    int count = 0;
+
+    //check all number:
+    for (int i = 1; i <= n; i++){
+        //reverse:
+        int temp = i;
+        int rem;
+        int sum = 0;
+        while(temp != 0){
+           rem = temp % 10;
+           sum+=rem*rem*rem;
+           temp/=10;
+        }
+        if(i == sum){
+            printf("%d ",i);
+            count++;
+        }
+
+    }
+    printf("\nTotal Armstrong number is %d",count);
+    return 0;
+}
