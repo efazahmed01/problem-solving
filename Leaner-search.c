bef37eb63a84d7fc5fa701a7770ae746f8bdb292
Efaz Ahmed <efazahmed.com@gmail.com>
2024-09-01 18:40:41 +0600

    Update Leaner-search.c

--- Leaner-search.c
+++ Leaner-search.c
@@ -1,3 +1,54 @@
+/*
+Time Complexity: O(n) 
+Space Complexity: O(1)
+*/
+#include<stdio.h>
+
+int linear_search(int a[],int n,int x)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == x)
+        {
+            return i;
+        }
+        
+    }
+    
+     i = -1;
+     return i;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    int f;
+    scanf("%d",&f);
+    
+    int index = linear_search(arr,n,f);
+    
+    if(index > 0)
+    {
+        printf("Found");
+    }
+    
+    else printf("Not Found");
+    
+    return 0;
+}
+
+/*
+without function:
+
 #include<stdio.h>
 int main()
 {
@@ -32,3 +83,4 @@ int main()
 
     return 0;
 }
+*/
