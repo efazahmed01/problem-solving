15ccdfbedfca428115a240a0bad94093cc01a63a
Efaz Ahmed <efazahmed.com@gmail.com>
2024-11-26 23:23:10 +0600

    Update programmer-s-meetup.cpp

--- Toph.co/programmer-s-meetup.cpp
+++ Toph.co/programmer-s-meetup.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 using namespace std;
 
-int main() {
-    string s; cin >> s;
+int main() 
+{
+        string s; cin >> s;
 	int n; cin >> n;
 	string s1; cin >> s1;
 	int n1; cin >> n1;
@@ -21,20 +22,20 @@ int main() {
 	cin >> t;
 	int sum = 0;
 	while(t--)
-		{
-			string h;
-			cin >> h;
-			if(h == s) sum+=n;
-			else if(h == s1) sum+=n1;
-			else if(h == s2) sum+=n2;
-			else if(h == s3) sum+=n3;
-			else if(h == s4) sum+=n4;
-		}
-	cout << "XXL " << sum/90 << endl;
-	sum%=90;
-    cout << "XL " << sum/30 << endl;
-    sum%=30;
-    cout << "L " << sum/10 << endl;
+	{
+		string h;
+		cin >> h;
+		if(h == s) sum+=n;
+		else if(h == s1) sum+=n1;
+		else if(h == s2) sum+=n2;
+		else if(h == s3) sum+=n3;
+		else if(h == s4) sum+=n4;
+	}
+        cout << "XXL " << sum/90 << endl;
+        sum%=90;
+        cout << "XL " << sum/30 << endl;
+        sum%=30;
+        cout << "L " << sum/10 << endl;
 	
 	return 0;
 }
